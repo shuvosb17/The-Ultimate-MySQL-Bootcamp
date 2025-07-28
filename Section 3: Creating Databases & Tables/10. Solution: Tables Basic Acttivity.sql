f630272e3ddd9970a67dd8f401c936aae79a0a4f
Shuvo <129549247+shuvosb17@users.noreply.github.com>
2025-07-28 18:58:55 +0600

    Create 10. Solution: Tables Basic Acttivity.sql

---
+++ Section 3: Creating Databases & Tables/10. Solution: Tables Basic Acttivity.sql
@@ -0,0 +1,19 @@
+--SOLUTION: Tables Basics Activity
+--Create the table:
+
+CREATE TABLE pastries
+  (
+    name VARCHAR(50),
+    quantity INT
+  );
+--View tables:
+
+SHOW TABLES;
+
+--View details of pastries table:
+
+DESC pastries;
+
+--Delete the whole pastries table:
+
+DROP TABLE pastries;
