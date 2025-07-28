0482cda36202793fd9bbb0174c76c7fa7123aaee
Shuvo <129549247+shuvosb17@users.noreply.github.com>
2025-07-28 18:39:30 +0600

    Create 7. Creating Tables.sql

---
+++ Section 3: Creating Databases & Tables/7. Creating Tables.sql
@@ -0,0 +1,13 @@
+--CODE: Creating Tables
+--Creating Tables:
+
+CREATE TABLE cats (        --For cats Table under pets_shop database
+    name VARCHAR(50),
+    age INT
+);
+ 
+CREATE TABLE dogs (      --for dogs Table under same pets_shop database
+    name VARCHAR(50),
+    breed VARCHAR(50),
+    age INT
+);
