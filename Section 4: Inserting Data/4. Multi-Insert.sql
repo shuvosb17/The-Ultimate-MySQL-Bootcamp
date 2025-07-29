09827b89ddb56524060cce060ef178e1c66ec521
Shuvo <129549247+shuvosb17@users.noreply.github.com>
2025-07-29 10:24:54 +0600

    Create 4. Multi-Insert.sql

---
+++ Section 4: Inserting Data/4. Multi-Insert.sql
@@ -0,0 +1,15 @@
+--CODE: Multi-inserts
+-- Single insert (switching order of name and age)
+
+INSERT INTO cats (age, name) 
+VALUES 
+  (2, 'Beth');
+
+
+-- Multiple Insert:
+
+INSERT INTO cats (name, age) 
+VALUES 
+  ('Meatball', 5), 
+  ('Turkey', 1), 
+  ('Potato Face', 15);
