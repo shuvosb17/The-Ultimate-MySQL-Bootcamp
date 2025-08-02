8e8ff03f6524c34f937a94502a8fd88b330ec2cc
Shuvo <129549247+shuvosb17@users.noreply.github.com>
2025-08-02 19:11:58 +0600

    Create 9.2. DELETE Exercise.sql

---
+++ Section 5: CRUD Basics/9.2. DELETE Exercise.sql
@@ -0,0 +1,12 @@
+--SOLUTION CODE
+-- Delete all 4 year old cats:
+
+DELETE FROM cats WHERE age=4;
+-- Delete cats where cat_id is the same as their age:
+
+DELETE FROM cats WHERE cat_id=age;
+
+
+-- Delete all cats:
+
+DELETE FROM cats;
