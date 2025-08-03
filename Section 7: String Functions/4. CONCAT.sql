165e4b91f99b9512d1e538761baa9892cea21510
Shuvo <129549247+shuvosb17@users.noreply.github.com>
2025-08-03 18:34:38 +0600

    Create 4. CONCAT.sql

---
+++ Section 7: String Functions/4. CONCAT.sql
@@ -0,0 +1,6 @@
+--CODE
+SELECT CONCAT('pi', 'ckle');
+ 
+SELECT CONCAT(author_fname,' ', author_lname) AS author_name FROM books;
+ 
+SELECT CONCAT_WS('-',title, author_fname, author_lname) FROM books;
