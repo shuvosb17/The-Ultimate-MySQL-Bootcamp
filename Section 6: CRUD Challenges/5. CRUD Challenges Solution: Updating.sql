64d6c0ae259a0635a4919c00db051a9fd33438f4
Shuvo <129549247+shuvosb17@users.noreply.github.com>
2025-08-03 08:35:15 +0600

    Create 5. CRUD Challenges Solution: Updating.sql

---
+++ Section 6: CRUD Challenges/5. CRUD Challenges Solution: Updating.sql
@@ -0,0 +1,21 @@
+--SOLUTION CODE
+UPDATE shirts 
+SET 
+    shirt_size = 'L'
+WHERE
+    article = 'polo shirt';
+    
+    
+UPDATE shirts 
+SET 
+    last_worn = 0
+WHERE
+    last_worn = 15;
+    
+    
+UPDATE shirts 
+SET 
+    color = 'off white',
+    shirt_size = 'XS'
+WHERE
+    color = 'white';
