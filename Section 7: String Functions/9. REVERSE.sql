7e1348780aa52daa2cb6ac34e0ff0d62eecec486
Shuvo <129549247+shuvosb17@users.noreply.github.com>
2025-08-12 19:39:41 +0600

    Create 9. REVERSE.sql

---
+++ Section 7: String Functions/9. REVERSE.sql
@@ -0,0 +1,10 @@
+--CODE
+SELECT REVERSE('Hello World');
+ 
+SELECT REVERSE('meow meow');
+ 
+SELECT REVERSE(author_fname) FROM books;
+ 
+SELECT CONCAT('woof', REVERSE('woof'));
+ 
+SELECT CONCAT(author_fname, REVERSE(author_fname)) FROM books;
