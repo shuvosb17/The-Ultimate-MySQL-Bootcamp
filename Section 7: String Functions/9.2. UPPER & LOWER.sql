209ea87a69017882565e494cc4b42f31cd18c914
Shuvo <129549247+shuvosb17@users.noreply.github.com>
2025-08-13 00:05:41 +0600

    Create 9.2. UPPER & LOWER.sql

---
+++ Section 7: String Functions/9.2. UPPER & LOWER.sql
@@ -0,0 +1,10 @@
+--CODE
+SELECT UPPER('Hello World');
+ 
+SELECT LOWER('Hello World');
+ 
+SELECT UPPER(title) FROM books;
+ 
+SELECT CONCAT('MY FAVORITE BOOK IS ', UPPER(title)) FROM books;
+ 
+SELECT CONCAT('MY FAVORITE BOOK IS ', LOWER(title)) FROM books;
