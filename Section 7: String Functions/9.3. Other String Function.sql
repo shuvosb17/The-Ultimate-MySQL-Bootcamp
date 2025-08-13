e97c959238db936ae2bdaa1c73ec4eaa5223719b
Shuvo <129549247+shuvosb17@users.noreply.github.com>
2025-08-13 16:49:29 +0600

    Update 9.3. Other String Function.sql

--- Section 7: String Functions/9.3. Other String Function.sql
+++ Section 7: String Functions/9.3. Other String Function.sql
@@ -8,3 +8,49 @@ SELECT RIGHT('omghahalol!', 4);
 SELECT REPEAT('ha', 4);
  
 SELECT TRIM('  pickle  ');
+
+
+
+mysql> SELECT INSERT('Hello Bobby', 6, 0, 'There');
++--------------------------------------+
+| INSERT('Hello Bobby', 6, 0, 'There') |
++--------------------------------------+
+| HelloThere Bobby                     |
++--------------------------------------+
+1 row in set (0.00 sec)
+
+mysql>
+mysql> SELECT LEFT('omghahalol!', 3);
++------------------------+
+| LEFT('omghahalol!', 3) |
++------------------------+
+| omg                    |
++------------------------+
+1 row in set (0.00 sec)
+
+mysql>
+mysql> SELECT RIGHT('omghahalol!', 4);
++-------------------------+
+| RIGHT('omghahalol!', 4) |
++-------------------------+
+| lol!                    |
++-------------------------+
+1 row in set (0.00 sec)
+
+mysql>
+mysql> SELECT REPEAT('ha', 4);
++-----------------+
+| REPEAT('ha', 4) |
++-----------------+
+| hahahaha        |
++-----------------+
+1 row in set (0.00 sec)
+
+mysql>
+mysql> SELECT TRIM('  pickle  ');
++--------------------+
+| TRIM('  pickle  ') |
++--------------------+
+| pickle             |
++--------------------+
+1 row in set (0.00 sec)
