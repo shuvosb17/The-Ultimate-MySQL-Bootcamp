899973e93adb307fe1f556604ac8bddd51b07269
Shuvo <129549247+shuvosb17@users.noreply.github.com>
2025-08-13 16:44:20 +0600

    Update 6. Combining String Function.sql

--- Section 7: String Functions/6. Combining String Function.sql
+++ Section 7: String Functions/6. Combining String Function.sql
@@ -5,3 +5,26 @@ SELECT CONCAT
         '...'
     ) AS 'short title'
 FROM books;
+
+
++---------------+
+| short title   |
++---------------+
+| The Namesa... |
+| Norse Myth... |
+| American G... |
+| Interprete... |
+| A Hologram... |
+| The Circle... |
+| The Amazin... |
+| Just Kids...  |
+| A Heartbre... |
+| Coraline...   |
+| What We Ta... |
+| Where I'm ... |
+| White Nois... |
+| Cannery Ro... |
+| Oblivion: ... |
+| Consider t... |
++---------------+
+16 rows in set (0.00 sec)
