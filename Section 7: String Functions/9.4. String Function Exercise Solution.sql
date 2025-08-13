8883f52b3ce3973bc6f0178ea3297ed13e28c810
Shuvo <129549247+shuvosb17@users.noreply.github.com>
2025-08-13 22:56:37 +0600

    Create 9.4. String Function Exercise Solution.sql

---
+++ Section 7: String Functions/9.4. String Function Exercise Solution.sql
@@ -0,0 +1,189 @@
+--SOLUTION CODE
+SELECT REVERSE(UPPER('Why does my cat look at me with such hatred?'));
+ 
+SELECT REPLACE(title, ' ', '->') AS title FROM books;
+ 
+SELECT 
+    author_lname AS forwards, REVERSE(author_lname) AS backwards
+FROM
+    books;
+    
+ 
+SELECT UPPER(CONCAT(author_fname, ' ', author_lname)) AS 'full name in caps' FROM books;
+ 
+ 
+SELECT CONCAT(title, ' was released in ', released_year) AS blurb FROM books;
+ 
+SELECT title, CHAR_LENGTH(title) AS character_count FROM books;
+ 
+SELECT 
+    CONCAT(SUBSTR(title, 1, 10), '...') AS short_title,
+    CONCAT(author_lname, ',', author_fname) AS author,
+    CONCAT(stock_quantity, ' in stock') AS quantity
+FROM
+    books;
+
+
+
+
+mysql> SELECT REVERSE(UPPER('Why does my cat look at me with such hatred?'));
++----------------------------------------------------------------+
+| REVERSE(UPPER('Why does my cat look at me with such hatred?')) |
++----------------------------------------------------------------+
+| ?DERTAH HCUS HTIW EM TA KOOL TAC YM SEOD YHW                   |
++----------------------------------------------------------------+
+1 row in set (0.00 sec)
+
+mysql>
+mysql> SELECT REPLACE(title, ' ', '->') AS title FROM books;
++--------------------------------------------------------------+
+| title                                                        |
++--------------------------------------------------------------+
+| The->Namesake                                                |
+| Norse->Mythology                                             |
+| American->Gods                                               |
+| Interpreter->of->Maladies                                    |
+| A->Hologram->for->the->King:->A->Novel                       |
+| The->Circle                                                  |
+| The->Amazing->Adventures->of->Kavalier->&->Clay              |
+| Just->Kids                                                   |
+| A->Heartbreaking->Work->of->Staggering->Genius               |
+| Coraline                                                     |
+| What->We->Talk->About->When->We->Talk->About->Love:->Stories |
+| Where->I'm->Calling->From:->Selected->Stories                |
+| White->Noise                                                 |
+| Cannery->Row                                                 |
+| Oblivion:->Stories                                           |
+| Consider->the->Lobster                                       |
++--------------------------------------------------------------+
+16 rows in set (0.00 sec)
+
+mysql>
+mysql> SELECT
+    ->     author_lname AS forwards, REVERSE(author_lname) AS backwards
+    -> FROM
+    ->     books;
++----------------+----------------+
+| forwards       | backwards      |
++----------------+----------------+
+| Lahiri         | irihaL         |
+| Gaiman         | namiaG         |
+| Gaiman         | namiaG         |
+| Lahiri         | irihaL         |
+| Eggers         | sreggE         |
+| Eggers         | sreggE         |
+| Chabon         | nobahC         |
+| Smith          | htimS          |
+| Eggers         | sreggE         |
+| Gaiman         | namiaG         |
+| Carver         | revraC         |
+| Carver         | revraC         |
+| DeLillo        | olliLeD        |
+| Steinbeck      | kcebnietS      |
+| Foster Wallace | ecallaW retsoF |
+| Foster Wallace | ecallaW retsoF |
++----------------+----------------+
+16 rows in set (0.00 sec)
+
+mysql>
+mysql>
+mysql> SELECT UPPER(CONCAT(author_fname, ' ', author_lname)) AS 'full name in caps' FROM books;
++----------------------+
+| full name in caps    |
++----------------------+
+| JHUMPA LAHIRI        |
+| NEIL GAIMAN          |
+| NEIL GAIMAN          |
+| JHUMPA LAHIRI        |
+| DAVE EGGERS          |
+| DAVE EGGERS          |
+| MICHAEL CHABON       |
+| PATTI SMITH          |
+| DAVE EGGERS          |
+| NEIL GAIMAN          |
+| RAYMOND CARVER       |
+| RAYMOND CARVER       |
+| DON DELILLO          |
+| JOHN STEINBECK       |
+| DAVID FOSTER WALLACE |
+| DAVID FOSTER WALLACE |
++----------------------+
+16 rows in set (0.00 sec)
+
+mysql>
+mysql>
+mysql> SELECT CONCAT(title, ' was released in ', released_year) AS blurb FROM books;
++--------------------------------------------------------------------------+
+| blurb                                                                    |
++--------------------------------------------------------------------------+
+| The Namesake was released in 2003                                        |
+| Norse Mythology was released in 2016                                     |
+| American Gods was released in 2001                                       |
+| Interpreter of Maladies was released in 1996                             |
+| A Hologram for the King: A Novel was released in 2012                    |
+| The Circle was released in 2013                                          |
+| The Amazing Adventures of Kavalier & Clay was released in 2000           |
+| Just Kids was released in 2010                                           |
+| A Heartbreaking Work of Staggering Genius was released in 2001           |
+| Coraline was released in 2003                                            |
+| What We Talk About When We Talk About Love: Stories was released in 1981 |
+| Where I'm Calling From: Selected Stories was released in 1989            |
+| White Noise was released in 1985                                         |
+| Cannery Row was released in 1945                                         |
+| Oblivion: Stories was released in 2004                                   |
+| Consider the Lobster was released in 2005                                |
++--------------------------------------------------------------------------+
+16 rows in set (0.00 sec)
+
+mysql>
+mysql> SELECT title, CHAR_LENGTH(title) AS character_count FROM books;
++-----------------------------------------------------+-----------------+
+| title                                               | character_count |
++-----------------------------------------------------+-----------------+
+| The Namesake                                        |              12 |
+| Norse Mythology                                     |              15 |
+| American Gods                                       |              13 |
+| Interpreter of Maladies                             |              23 |
+| A Hologram for the King: A Novel                    |              32 |
+| The Circle                                          |              10 |
+| The Amazing Adventures of Kavalier & Clay           |              41 |
+| Just Kids                                           |               9 |
+| A Heartbreaking Work of Staggering Genius           |              41 |
+| Coraline                                            |               8 |
+| What We Talk About When We Talk About Love: Stories |              51 |
+| Where I'm Calling From: Selected Stories            |              40 |
+| White Noise                                         |              11 |
+| Cannery Row                                         |              11 |
+| Oblivion: Stories                                   |              17 |
+| Consider the Lobster                                |              20 |
++-----------------------------------------------------+-----------------+
+16 rows in set (0.00 sec)
+
+mysql>
+mysql> SELECT
+    ->     CONCAT(SUBSTR(title, 1, 10), '...') AS short_title,
+    ->     CONCAT(author_lname, ',', author_fname) AS author,
+    ->     CONCAT(stock_quantity, ' in stock') AS quantity
+    -> FROM
+    ->     books;
++---------------+----------------------+--------------+
+| short_title   | author               | quantity     |
++---------------+----------------------+--------------+
+| The Namesa... | Lahiri,Jhumpa        | 32 in stock  |
+| Norse Myth... | Gaiman,Neil          | 43 in stock  |
+| American G... | Gaiman,Neil          | 12 in stock  |
+| Interprete... | Lahiri,Jhumpa        | 97 in stock  |
+| A Hologram... | Eggers,Dave          | 154 in stock |
+| The Circle... | Eggers,Dave          | 26 in stock  |
+| The Amazin... | Chabon,Michael       | 68 in stock  |
+| Just Kids...  | Smith,Patti          | 55 in stock  |
+| A Heartbre... | Eggers,Dave          | 104 in stock |
+| Coraline...   | Gaiman,Neil          | 100 in stock |
+| What We Ta... | Carver,Raymond       | 23 in stock  |
+| Where I'm ... | Carver,Raymond       | 12 in stock  |
+| White Nois... | DeLillo,Don          | 49 in stock  |
+| Cannery Ro... | Steinbeck,John       | 95 in stock  |
+| Oblivion: ... | Foster Wallace,David | 172 in stock |
+| Consider t... | Foster Wallace,David | 92 in stock  |
++---------------+----------------------+--------------+
+16 rows in set (0.00 sec)
